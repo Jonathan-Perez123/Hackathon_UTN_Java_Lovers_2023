4a0c8f876f97757888fb1427c733355b637c0d44
Jonathan Perez <113068258+Jonathan-Perez123@users.noreply.github.com>
2023-07-04 20:35:00 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,34 @@
-document.getElementById('contact').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Gracias por contactarnos. Pronto nos pondremos en contacto contigo.');
-  });
- 
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+// scripts.js
+
+// Seleccionamos todos los carruseles de la página.
+const carousels = document.querySelectorAll('.carousel');
+
+// Iteramos sobre cada carrusel.
+carousels.forEach((carousel) => {
+  let currentItem = 0;
+  const items = carousel.querySelectorAll('.carousel-item');
+
+  // Función para mostrar un elemento del carrusel.
+  function showItem(index) {
+    // Primero, ocultamos todos los elementos.
+    items.forEach((item, i) => {
+      item.style.display = 'none';
+      if (i === index) {
+        // Mostramos el elemento en el índice dado.
+        item.style.display = 'block';
+      }
     });
-  });
+  }
+
+  // Mostramos el primer elemento al cargar la página.
+  showItem(currentItem);
+
+  // Cada 4 segundos, avanzamos al siguiente elemento.
+  setInterval(() => {
+    currentItem = (currentItem + 1) % items.length; // El operador % (módulo) hace que el carrusel sea circular.
+    showItem(currentItem);
+  }, 4000); // 4000 milisegundos = 4 segundos.
+});
+
 
 
